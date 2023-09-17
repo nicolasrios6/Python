5d5ce6cdfac346d6ab2935b1ec1a52e631217b10
Nicolás Ríos <riosnicolas618@gmail.com>
2023-09-16 22:53:54 -0300

    Nueva estructura de proyecto

---
+++ 2_Intermediate/00_dates.py
@@ -0,0 +1,18 @@
+# Dates
+
+from datetime import datetime
+
+now = datetime.now()
+
+print(now.year)
+print(now.day)
+print(now.hour)
+print(now.minute)
+print(now.second)
+
+timestamp = now.timestamp()
+
+print(timestamp)
+
+
+
